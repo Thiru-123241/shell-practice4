b5ca87e1a9426674424feebc07d5c2a36a60e59c
Thirumalesh <thirumaleshvasfdc@gmail.com>
2025-10-17 23:20:05 +0530

    Added colours

--- colours3.sh
+++ colours3.sh
@@ -2,6 +2,6 @@
 
 
 echo -e  "\e[31mThiru : Hi bro whats up\e[0m"
-echo "\e[32msuresh : Hi Thiru i am fine too and u\e[0m"
-echo "\e[33mThiru : I am also fine what is going on\e[0m"
-echo "\e[34msuresh : Nothing is going on\e[0m"
+echo -e "\e[32msuresh : Hi Thiru i am fine too and u\e[0m"
+echo -e "\e[33mThiru : I am also fine what is going on\e[0m"
+echo -e "\e[34msuresh : Nothing is going on\e[0m"
--- colours4.sh
+++ colours4.sh
@@ -3,7 +3,7 @@
 
 
 echo -e  "\e[31mThiru : Hi bro whats up\e[0"
-echo "\e[32suresh : Hi Thiru i am fine too and u\e[0"
+echo -e "\e[32suresh : Hi Thiru i am fine too and u\e[0"
 echo "Thiru : I am also fine what is going on"
 echo "suresh : Nothing is going on"
 echo "Nothing"
