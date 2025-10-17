c826c6c1425a6d6faeebac25d9fb7e888388a023
Thirumalesh <thirumaleshvasfdc@gmail.com>
2025-10-17 23:16:33 +0530

    Added colours

---
+++ colours3.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+
+echo -e  "\e[31mThiru : Hi bro whats up\e[0m"
+echo "\e[32msuresh : Hi Thiru i am fine too and u\e[0m"
+echo "\e[33mThiru : I am also fine what is going on\e[0m"
+echo "\e[34msuresh : Nothing is going on\e[0m"
