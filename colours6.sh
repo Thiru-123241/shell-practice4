a7b4d42a4b0f8138d9a708e4d2665cc25cbb82c5
Thirumalesh <thirumaleshvasfdc@gmail.com>
2025-10-17 23:37:06 +0530

    Added colours

---
+++ colours6.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+
+echo -e  "\e[31mThiru : Hi bro whats up\e[0"
+echo -e "\e[34msuresh : Hi Thiru i am fine too and u"
+echo "Thiru : I am also fine what is going on"
+echo "suresh : Nothing is going on"
+echo "Nothing"
+echo "what are you doing"
+echo "what are you studying"
