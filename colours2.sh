bdaed19a53204ee248e475a1e5101d000904ebf7
Thirumalesh <thirumaleshvasfdc@gmail.com>
2025-10-17 22:53:17 +0530

    Added colours

---
+++ colours2.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+
+echo -e  "\e[31mThiru : Hi bro whats up\e[0m"
+echo "suresh : Hi Thiru i am fine too and u"
+echo "Thiru : I am also fine what is going on"
+echo "suresh : Nothing is going on"
