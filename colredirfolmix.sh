722af047f4bc2cfb56560d763d58c7c8c180f796
Thirumalesh <thirumaleshvasfdc@gmail.com>
2025-10-21 22:29:10 +0530

    Added mix

---
+++ colredirfolmix.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+Num=$1
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+Log_Folder="/var/log/shellscript"
+Script_name=$(echo $0 | cut -d "." -f1)
+Log_File="$Log_Folder/$Script_name.log"
+
+
+mkdir -p $Log_Folder
+echo "program starts at : $(date) &>> $Log_File"
+if [ $Num -gt 100 ]
+then
+   echo -e "$R$Num is greater than 100......... &>> $Log_File$N"
+else
+   echo -e "$G$Num is lessthan than 100........&>> $Log_File$N"
+fi  
+      
