ec9150bbd2558ae58660108a06fdff2443cab7cd
Thirumalesh <thirumaleshvasfdc@gmail.com>
2025-10-21 22:34:49 +0530

    Added mix

--- colredirfolmix.sh
+++ colredirfolmix.sh
@@ -10,11 +10,11 @@ Log_File="$Log_Folder/$Script_name.log"
 
 
 mkdir -p $Log_Folder
-echo "program starts at : $(date) &>> $Log_File"
+echo "program starts at : $(date)" &>> $Log_File
 if [ $Num -gt 100 ]
 then
-   echo -e "$R$Num is greater than 100......... &>> $Log_File$N"
+   echo -e "$R$Num is greater than 100.........$N" &>> $Log_File
 else
-   echo -e "$G$Num is lessthan than 100........&>> $Log_File$N"
+   echo -e "$G$Num is lessthan than 100........$N" &>> $Log_File
 fi  
       
