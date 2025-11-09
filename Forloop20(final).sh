1a8f6c034bcca083764e865505a47c2562f2ea34
Thirumalesh <thirumaleshvasfdc@gmail.com>
2025-11-09 23:19:35 +0530

    For loop added

---
+++ Forloop20(final).sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+R="\e[31m"
+G="\e[32m"
+B="\e[34m"
+M="\e[35m"
+N="\e[0m"
+Y="\e[33m"
+C="\e[36m"
+Log_Folder="/var/log/Nov"
+Script_name=$(echo $0 | cut -d "." -f1)
+Log_File="$Log_Folder/$Script_name.log"
+Time=$(date)
+mkdir -p $Log_Folder
+echo "program starts at : $Time" &>>$Log_File
+user=$(id -u)
+if [ $user -eq 0 ]
+then
+   echo -e "$R you are running with the root access only $N" | tee -a $Log_File
+else 
+   echo -e "$G Error:we need sudo access to run this script $N"  | tee -a $Log_File
+   exit 1
+fi
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+       echo -e "$B error : $2 package  installation is failure $N" | tee -a $Log_File
+       exit 1
+    else
+        echo -e "$M $2 package  installation is success $N" | tee -a $Log_File
+    fi
+}
+for i in $@
+do
+  dnf list installed $i &>>$Log_File
+ if [ $? -ne 0 ]
+ then
+   echo -e "$Y now we are ready to install $i $N" | tee -a $Log_File
+   dnf install $i -y &>>$Log_File
+  VALIDATE $?  $i
+ else
+   echo -e "$C $i package is already installed.......nothing to do now $N" | tee -a $Log_File
+ fi
+ done
